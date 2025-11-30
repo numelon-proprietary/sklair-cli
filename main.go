e3795f46953416689856abff8f064f93bfdc0952
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2025-11-30 05:11:27 +0000

    It works almost!

--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"os"
+	"path/filepath"
 	"sklair/logger"
 	"strings"
 
 	"golang.org/x/net/html"
 )
+
+func clone(n *html.Node) *html.Node {
+	if n == nil {
+		return nil
+	}
+
+	// hah
+	clown := &html.Node{
+		Type:     n.Type,
+		DataAtom: n.DataAtom,
+		Data:     n.Data,
+		Attr:     append([]html.Attribute{}, n.Attr...),
+	}
+
+	for child := n.FirstChild; child != nil; child = child.NextSibling {
+		clown.AppendChild(clone(clown))
+	}
+
+	return clown
+}
 
 func main() {
 	logger.InitShared(logger.LevelDebug)
@@ -35,6 +55,54 @@ func main() {
 		}
 	}
 
-	fmt.Println(toReplace)
 	logger.Info("Found %d tags to replace", len(toReplace))
+
+	for _, node := range toReplace {
+		componentPath := filepath.Join("components", node.Data+".html")
+
+		if _, err := os.Stat(componentPath); err != nil {
+			logger.Error("Could not find component %s : %s", componentPath, err.Error())
+			return
+		} else {
+			f, err := os.ReadFile(componentPath)
+			if err != nil {
+				logger.Error("Could not read component %s : %s", componentPath, err.Error())
+				return
+			}
+
+			component, err := html.Parse(bytes.NewReader(f))
+			if err != nil {
+				logger.Error("Could not parse component %s : %s", componentPath, err.Error())
+				return
+			}
+
+			// even though components are usually bare (without doctype, head, body, etc), we still need to find the "body" (bc parsed)
+			body := component.FirstChild
+			for body != nil && body.Data != "html" {
+				body = body.NextSibling
+			}
+			if body != nil {
+				body = body.FirstChild
+				for body != nil && body.Data != "body" {
+					body = body.NextSibling
+				}
+			}
+
+			if body != nil {
+				parent := node.Parent
+				if parent != nil {
+					for child := body.FirstChild; child != nil; child = child.NextSibling {
+						parent.InsertBefore(clone(child), node)
+					}
+					parent.RemoveChild(node)
+				}
+			}
+		}
+	}
+
+	newWriter := bytes.NewBuffer(nil)
+	html.Render(newWriter, doc)
+
+	os.WriteFile("output.html", newWriter.Bytes(), 0644)
+	logger.Info("Saved to output.html")
 }
