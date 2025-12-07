fba1e8de6046c4da68a5cb18fe2c42d4202da7f1
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2025-12-07 21:07:56 +0000

    Exclude more folders during document discovery

--- discovery/documents.go
+++ discovery/documents.go
@@ -14,6 +14,10 @@ type DocumentLists struct {
 var skipDirs = map[string]struct{}{
 	"components": {},
 	".git":       {},
+	".vscode":    {},
+	".idea":      {},
+	".env":       {}, // these are usually files for most people,
+	// but at Numelon these are folders and therefore must be excluded (for safety)
 }
 
 func DocumentDiscovery(root string) (*DocumentLists, error) {
@@ -28,12 +32,13 @@ func DocumentDiscovery(root string) (*DocumentLists, error) {
 			return filepath.SkipDir
 		}
 
-		// skip directories since they will be walked by filepath.Walk later anyways
+		// skip directories since they will be walked by filepath.Walk later anyway
 		if info.IsDir() {
 			return nil
 		}
 
 		ext := strings.ToLower(filepath.Ext(info.Name()))
+		// TODO: perhaps allow this file ext to be customisable?
 		if ext == ".html" || ext == ".shtml" {
 			lists.HtmlFiles = append(lists.HtmlFiles, path)
 		} else {
